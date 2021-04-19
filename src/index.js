0d72c2578be137039c4a9cc837b4296169a245e5
dracero <diego.racero@gmail.com>
2021-04-19 20:50:15 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,6 +20,6 @@ const server = new ApolloServer({
   resolvers,
 })
 
-server.listen({ port: process.env.PORT || 4000 })
-  .then(({ url }) => {console.log(`🚀 Server ready at ${url}`);
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
 });
